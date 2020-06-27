c8153c9ba67529f72f5dfbe9fdf4791fa9cd0941
Lutencjusz <lutencjusz@gmail.com>
2020-06-27 20:30:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To jest przykładowa aplikcja</h1>
+        <h3>Pokazuje działanie useSVR oraz tabeli</h3>
       </header>
-    </div>
+      <body>
+        <h2>Przykład</h2>
+      </body>
+    </Fragment>
   );
 }
 
